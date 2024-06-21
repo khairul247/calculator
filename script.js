3c28f8419993920f54f08a8f23b226c8df956fe8
Muhammad Khairul Ikhwan <khairulikhwan@Muhammads-MacBook-Air.local>
2024-06-21 23:48:12 +0800

    Complete the number and operator function

--- script.js
+++ script.js
@@ -1,7 +1,79 @@
-const add = ((a,b) => a+b)
 
-const subtract = ((a,b) => a-b)
+// const and var section
+const operator = {
+  add: (a,b) => a+b,
+  subtract: (a,b) => a-b,
+  multiply: (a,b) => a*b,
+  divide: (a,b) => b === 0 ? 'How Dare You!' : a/b
+}
 
-const multiply = ((a,b) => a*b)
+const numberButtons = document.querySelectorAll('.numberButtons button');
+const operatorButtons = document.querySelectorAll('.operatorButtons button');
+const equalButton = document.querySelector('#equal')
+const display = document.querySelector('#display');
 
-const divide = ((a,b) => b === 0 ? 'How Dare You!' : a/b)
+let currentNumber = "";
+let operatorCounter = 0;
+let result = 0;
+let currentId = "";
+let previousId = "";
+let a = 0;
+let b = 0;
+let isFirstNumberEntered = false;
+
+// event listener section
+
+numberButtons.forEach(button =>{
+  button.addEventListener('click', (event) => {
+    currentNumber += event.target.textContent;
+    display.textContent = currentNumber;
+
+    if (isFirstNumberEntered){
+      b = Number(currentNumber);
+    } else{
+      a = Number(currentNumber);
+    }
+
+  })
+})
+
+operatorButtons.forEach(button => {
+  button.addEventListener('click', (event) =>{
+    isFirstNumberEntered = true;
+    operatorCounter++
+    handleOperator(operatorCounter,event);
+  })
+})
+
+equalButton.addEventListener('click', ()=>{
+  result = operator[previousId](a,b);
+  a = result;
+  display.textContent = result;
+  operatorCounter = 0;
+
+})
+
+// function section
+
+function operate(a,b,operator){
+  let value = operator(a,b);
+  return value;
+}
+
+function handleOperator(operatorCounter,event){
+if (operatorCounter > 1){
+  console.log(previousId)
+  currentId = event.target.id;
+  result = operator[previousId](a,b);
+  previousId = currentId;
+  console.log(result)
+  a = result;
+  display.textContent = a;
+  currentNumber = '';
+
+} else{
+  previousId = event.target.id;
+  currentNumber = '';
+  console.log(previousId)
+}
+}
